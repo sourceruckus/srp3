ca4519f8f042fc8ced1ccdf4c426a917a659f9af
Michael D Labriola <mdlabriola@yahoo.com>
2012-03-04 14:36:46 -0500

    init: tidied up more arg parsing

--- dev-3.0/srp/features/init.py
+++ dev-3.0/srp/features/init.py
@@ -1,6 +1,7 @@
 """main program"""
 
 import argparse
+import sys
 
 
 prog = "The Source Ruckus Packager"
@@ -13,9 +14,6 @@ desc = """\
 """ % (prog, version, build_year)
 
 epi = """\
-NOTE: passing in '-' instead of a package name will cause srp to read missing
-      arguments from stdin.
-
 example: srp -vvv --install=strip_debug,strip_docs,strip_man something.i686.brp
 
 example: srp --build something.srp
@@ -79,13 +77,38 @@ g.add_argument('-a', '--action', metavar="ACTIONS",
                     performed (e.g.,
                     --action=strip_debug,strip_docs,commit).""")
 
-# FIXME: should PATTERN be a regular expression or a unix glob-style
-#        fnmatch?  default should be either '.*' or '*' depending...
+# FIXME: need to document supported actions somewhere.  here's a list of the
+#        planned ones for now:
 #
-# FIXME: for now, i'm leaning towards fnmatch.fileter to get a list of
-#        matching packages.
-g.add_argument('-l', '--list', metavar="PATTERN", nargs='?',
-               const="*",
+#        strip_debug - run strip --strip-unneeded on all installed files
+#
+#        strip_doc - remove installed files in PREFIX/share/doc
+#
+#        strip_man - remove installed manpages (PREFIX/share/man/)
+#
+#        strip_info - remove installed info pages (PREFIX/share/info/)
+#
+#        strip_locale - remove all internationalization translation files
+#        (PREFIX/share/locale)
+#
+#        strip_all - alias to list of all supported strip_* actions
+#
+#        repair - revert any modified files back to their installed state
+#
+#        add_file=file - add the specified file to the package's file list
+#
+#        rm_file=file - remove the specified file from the package's file
+#        list
+#
+#        add_dep_prog=file
+#        rm_dep_prog=file
+#
+#        update_dep_libs - recalculate package library deps by scanning the
+#        (potentially updated) list of installed files.
+#
+#        commit - re-checksum and record package changes
+
+g.add_argument('-l', '--list', metavar="PATTERN", nargs='*',
                help="""List installed packages matching Unix shell-style
                     wildcard PATTERN (or all packages if PATTERN not
                     supplied).""")
@@ -99,7 +122,7 @@ g.add_argument('-V', '--version', action='version',
 
 # the following options are independent of the exclusive group (at least as
 # far as the ArgumentParser is concerned).
-p.add_argument('-v', '--verbose', action='count',
+p.add_argument('-v', '--verbose', action='count', default=0,
                help="""Be verbose.  Can be supplied multiple times for
                     increased levels of verbosity.""")
 
@@ -122,12 +145,84 @@ p.add_argument('-p', '--package', metavar='PACKAGE', nargs='+',
                --query, --action, and --build.  Note that PACKAGE can be a
                Unix shell-style wildcard for modes that act on previously
                installed packages (e.g., --uninstall, --query, --action).
-               If left out, packages are expected on stdin.""")
+               If a specified PACKAGE is '-', additional PACKAGEs are read
+               from stdin.  If --package is left out entirely, packages are
+               expected on stdin.""")
 
-args = p.parse_args()
+
+# FIXME: should i be able to augment package list via stdin? I.e., use
+#        --package AND also read more package from stdin.
+def get_package_list():
+    retval = []
+
+    if args.package:
+        retval.extend(args.package)
+        if '-' not in retval:
+            return retval
+        else:
+            retval.remove('-')
+
+    # FIXME: for now, we assume that stdin is set up as a pipe if no
+    #        packages were specified or if one of the specified pacakges is
+    #        '-'.  would be nice if we could tell if this was going to
+    #        block...
+    retval.extend(sys.stdin.read().split())
+    return retval
 
 
-print(args)
+if __name__ == "__main__":
+    args = p.parse_args()
 
-if args.install:
-    print("install")
+    print(args)
+
+    # FIXME: should i just add force as a per-method option instead of a
+    #        global option? i.e., --install=strip_debug,force instead of
+    #        --install=strip_debug --force.
+    #
+    # FIXME: kinda undecided, but I think i like --force better. feels a
+    #        little strange in some cases (e.g., --action's argument is
+    #        called ACTIONS and force would not be an action... it's an
+    #        option)
+    #
+    # FIXME: but maybe we don't want to allow --force for --action...?  just
+    #        say that --action is for fixing things that would otherwise
+    #        require you to add --force to other comands?
+
+    #force = args.force
+    #verbose = args.verbose
+    #init = args.init
+
+    # FIXME: we should error out if metadata has not yet been initialized
+    #        (unless args.init is set, obviously)
+
+    print("do_init_output(level=%d)" % args.verbose)
+
+    if args.install:
+        for x in get_package_list():
+            print("do_install(package=%s, flags=%s)" % (x, args.install.split(',')))
+
+    elif args.uninstall:
+        for x in get_package_list():
+            print("do_uninstall(package=%s, flags=%s)" % (x, args.uninstall.split(',')))
+
+    elif args.build:
+        # NOTE: I'm not sure why you would end up specifying packages to
+        #       build on stdin... but we might as well support it
+        for x in get_package_list():
+            print("do_build(package=%s, flags=%s)" % (x, args.build.split(',')))
+
+    elif args.action:
+        for x in get_package_list():
+            print("do_action(package=%s, actions=%s)" % (x, args.action.split(',')))
+
+    elif args.query:
+        for x in get_package_list():
+            print("do_query(package=%s, fields=%s)" % (x, args.query.split(',')))
+
+    elif args.list != None:
+        if not args.list:
+            args.list = ["*"]
+        print("do_list(pattern='%s')" % (args.list))
+
+    elif args.init:
+        print("do_init_metadata()")
