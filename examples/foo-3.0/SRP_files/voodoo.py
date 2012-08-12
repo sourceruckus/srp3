8be3f9647ee4c98c378f4ea1fb093000d82d802d
Michael D Labriola <michael.d.labriola@gmail.com>
2012-08-12 14:23:05 -0400

    example: added new autotools-based 3.0 example
    The SRP_Files dir is copied straight out of the dev-3.0 tree for now.

---
+++ examples/foo-3.0/SRP_files/voodoo.py
@@ -0,0 +1,23 @@
+def preinstall():
+    try:
+        pass
+    except Exception, e:
+        raise Exception("failed to do some stuff: %s" % e)
+
+def postinstall():
+    try:
+        pass
+    except Exception, e:
+        raise Exception("failed to do some stuff: %s" % e)
+
+def preuninstall():
+    try:
+        pass
+    except Exception, e:
+        raise Exception("failed to do some stuff: %s" % e)
+
+def postuninstall():
+    try:
+        pass
+    except Exception, e:
+        raise Exception("failed to do some stuff: %s" % e)
