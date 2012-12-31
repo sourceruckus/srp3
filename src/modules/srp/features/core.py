0eaae9c3d4a6b62032d49b596f96fa83dea377e0
Michael D Labriola <michael.d.labriola@gmail.com>
2012-12-31 18:43:44 +0000

    features/core: initial commit

---
+++ src/modules/srp/features/core.py
@@ -0,0 +1,35 @@
+"""Feature module for the core functionality of SRP.
+
+This feature module implements the core functionality of the package manager
+(i.e., creating, building, installing packages).
+"""
+
+from srp.features import *
+
+def create_func():
+    """create tar of NOTES, source, SHA"""
+    pass
+
+def build_func():
+    """run build script to create tar of payload"""
+    pass
+
+def install_func():
+    """untar payload, install tarinfo in ruckus/installed/pkgname/sha"""
+    pass
+
+def uninstall_func():
+    """remove files listed in pkg manifest"""
+    pass
+
+def commit_func():
+    """update pkg manifest"""
+    pass
+
+register_feature(feature_struct("core",
+                                __doc__,
+                                create = stage_struct("core", create_func, [], []),
+                                build = stage_struct("core", build_func, [], []),
+                                install = stage_struct("core", install_func, [], []),
+                                uninstall = stage_struct("core", uninstall_func, [], []),
+                                action = ("commit", stage_struct("core", commit_func, [], []))))
