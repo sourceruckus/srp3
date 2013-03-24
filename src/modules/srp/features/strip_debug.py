916fe5ec6a722338714b0965fed9f07f197841e3
Michael D Labriola <michael.d.labriola@gmail.com>
2013-03-24 16:30:25 -0400

    features: added strip_debug feature module

---
+++ src/modules/srp/features/strip_debug.py
@@ -0,0 +1,38 @@
+"""Feature module for stripping debug symbols at install-time
+
+This feature strips debug symbols (all unneeded symbols, actually) at
+install-time, prior to checksums getting recorded.
+"""
+
+from srp.features import *
+
+# NOTE: This has to happen AFTER core (i.e., after the files are installed
+#       on the system) because we can't modify the files in the BLOB archive.
+#
+# FIXME: We COULD change this, but we'd have to add new, stripped versions
+#        of all files back to the BLOB archive... or create a new one
+#        entirely and replace it prior to running core...  We'll see.
+#
+#        It would be nice to be able to do it prior to core, if we can make
+#        it work efficiently, because that way the user won't have to have
+#        as much disk space available at install time.  And if stripping
+#        fails, we won't have a half-installed package on our hands...
+#
+#        Although, a hidden benefit of doing it after core is that the
+#        install_func and our strip_debug action can use the exact same
+#        function.
+def install_func(work):
+    """strip --strip-unneeded from all files"""
+    pass
+
+
+# FIXME: this should also register an action so we can stip after
+#        installation if we want to
+
+register_feature(feature_struct("strip_debug",
+                                __doc__,
+                                False,
+                                install = stage_struct("strip_debug", install_func, ["core"], ["?checksum"]),
+                                action = [("strip_debug", stage_struct("strip_debug", install_func, [], []))]))
+
+# FIXME: what happens if checksum isn't enabled?
