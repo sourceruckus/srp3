a02a0e2468b08156f77bd3ddf07c06b532dfbe9d
Michael D Labriola <michael.d.labriola@gmail.com>
2015-12-11 14:36:25 -0500

    examples: added a libtool library

--- examples/foo-3.0/src/foo.cpp
+++ examples/foo-3.0/src/foo.cpp
@@ -1,12 +1,18 @@
+// -*- c-file-style: "k&r"; indent-tabs-mode: nil -*-
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <cstring>
 #include <iostream>
-#include <string>
 using namespace std;
+
+#include "config.h"
 
 
 int main(int argc, char **argv)
 {
-    string version = "__VERSIONSTRING__";
-    cout << "foo " << version << ": hello, world" << endl;
-    return(0);
+    cout << basename(argv[0]) << " " << VERSION << ": hello, world" << endl;
+    return 0;
 }
 
---
+++ examples/foo-3.0/src/pizza.c
@@ -0,0 +1,18 @@
+// -*- c-file-style: "k&r"; indent-tabs-mode: nil -*-
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <stdio.h>
+#include <string.h>
+
+#include "config.h"
+#include "crust.h"
+
+
+int main(int argc, char **argv)
+{
+     printf("%s %s with %s crust: hello, world\n", basename(argv[0]), VERSION,
+            crustiness);
+     return 0;
+}
